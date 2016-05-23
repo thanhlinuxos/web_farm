ec23ef75ca785eaf18c36eda4587687ae00684a1
ThanhN <nthanh@yahoo-corp.jp>
2016-05-23 12:06:55 +0700

    Add login form

--- web/config/routes.php
+++ web/config/routes.php
@@ -2,11 +2,15 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $route['default_controller'] = 'frontend/dashboard';
-// ACP
+
+
+/******************** ACP *************************/
 $route['acp'] = 'backend/dashboard';
+$route['acp/login'] = 'backend/auth/login';
 
 //User
 $route['acp/user/add'] = 'backend/user/add';
 
+//Default router
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
