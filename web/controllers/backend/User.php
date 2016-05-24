a350a59535a1f2e3318759b57099756c34871b09
Thanh Nguyen <thanh.linuxos@gmail.com>
2016-05-24 22:41:13 +0700

    add lang

--- web/controllers/backend/User.php
+++ web/controllers/backend/User.php
@@ -6,6 +6,7 @@ class User extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
+        $this->lang->load('backend');
     }
 
     public function index() {
