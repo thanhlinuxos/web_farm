9cd1bdcbd9ae5865c106ac324632f630e322625e
ThanhN <nthanh@yahoo-corp.jp>
2016-06-21 14:41:40 +0700

    add pie canvas

--- web/views/backend/layout/dashboard.php
+++ web/views/backend/layout/dashboard.php
@@ -1,39 +1,46 @@
-<script type="text/javascript" src="<?php echo base_url('vendor/canvasjs/canvasjs.min.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('vendor/rgraph/libraries/RGraph.common.core.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('vendor/rgraph/libraries/RGraph.common.key.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('vendor/rgraph/libraries/RGraph.pie.js'); ?>"></script>
 <script type="text/javascript">
-    function chartUsers(data){
-        var chart = new CanvasJS.Chart("chartUsers",
-            {
-                theme: "theme3",
-                title: {
-                    text: "Biểu đồ tổng quát nhân viên"
-                },
-                data: [
-                    {
-                        type: "pie",
-                        showInLegend: true,
-                        toolTipContent: "{y} - #percent %",
-                        yValueFormatString: "#0.# người",
-                        legendText: "{indexLabel}",
-                        dataPoints: data
-                    }
-                ]
-            });
-        chart.render();
-        $('.canvasjs-chart-credit').hide();
-    }
+    function chart_users(input)
+    {
+        var data = [];
+        var labels = [];
+        input.forEach(function(item){
+            data.push(item.y);
+            labels.push(item.indexLabel);
+        });
+        // Create the Pie chart object
+        var pie = new RGraph.Pie({
+            id: 'cvs',
+            data: data
+        })
+        // Append the number to the labels
+        for (var i=0; i<data.length; ++i) {
+            labels[i] = labels[i] + ': ' + RGraph.numberFormat(pie, data[i]) + ' (người)';
+        }
+        pie.set({
+            labels: labels,
+            labelsSticksList: true,
+            strokestyle: 'white',
+            linewidth: 1,
+            textAccessible: true
+        }).draw();
+    };
+    
     $(document).ready(function(){
         $.ajax({
             url: BASE_URL + '/acp/dashboard/charts',
             method: 'POST',
             data: {type: 'user'},
             success: function(response, status){
-                chartUsers(JSON.parse(response));
+                chart_users(JSON.parse(response));
             }
         });
     });
 </script>
 <div class="row">
     <div class="col-xs-12">
-        <div id="chartUsers" style="height: 400px; width: 100%;"></div>
+        <canvas id="cvs" width="700" height="325">[No canvas support]</canvas>
     </div>
 </div>
